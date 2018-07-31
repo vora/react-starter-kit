3fb431dddf15ac5ea2edad15a9f7a12195923481
knipferrc <knipferrc@gmail.com>
2018-07-31 08:13:18 -0400

    improvements

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,21 @@
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+const convert = require('koa-connect')
+const Dotenv = require('dotenv-webpack')
+const history = require('connect-history-api-fallback')
 const HtmlWebPackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const path = require('path')
-const history = require('connect-history-api-fallback')
-const convert = require('koa-connect')
-const Dotenv = require('dotenv-webpack')
 
 module.exports = {
   mode: process.env.NODE_ENV ? 'development' : 'production',
   output: {
-    filename: '[name].[hash].js'
+    filename: '[name].[hash].js',
+    publicPath: '/'
   },
   serve: {
+    content: ['./src'],
+    open: true,
+    port: 3000,
     add: app => {
       app.use(convert(history()))
     }
@@ -58,8 +63,9 @@ module.exports = {
     ]
   },
   plugins: [
+    new CleanWebpackPlugin(['dist']),
     new HtmlWebPackPlugin({
-      template: './src/index.html',
+      template: './index.html',
       filename: './index.html',
       minify: process.env.NODE_ENV === 'production',
       favicon: path.join(__dirname, './favicon.ico')
