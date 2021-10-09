b8efe18f24dc691c555cadb8c8174d5346f67ca6
knipferrc <knipferrc@gmail.com>
2021-10-08 22:48:48 -0400

    use react plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
-import reactRefresh from '@vitejs/plugin-react-refresh';
+import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh(), svgr()],
+  plugins: [react(), svgr()],
 });
